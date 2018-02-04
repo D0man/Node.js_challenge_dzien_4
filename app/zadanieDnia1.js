952c6982b118247472dfbaacf5b982954df67139
d0man <jakub.tomasz.domanski@gmail.com>
2018-02-04 17:37:41 +0100

    day four

--- app/zadanieDnia1.js
+++ app/zadanieDnia1.js
@@ -1,1 +1,10 @@
-//Twój kod
+const http = require('http');
+
+const srv = http.createServer((req, res) => {
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.end("<h1>Hello, World from back-end!</h1>");
+});
+
+srv.listen(3000, () => {
+    console.log('Serwer uruchomiony na porcie 3000');
+});
